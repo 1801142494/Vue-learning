aff600fd33690e2e92bb39bbd414cb9da03e89ac
ChangChun <88582433+1801142494@users.noreply.github.com>
2024-08-24 23:48:20 +0800

    Add files via upload

---
+++ Vue2/19-单文件组件/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <School></School>
+        <Student></Student>
+    </div>
+</template>
+
+<script>
+    // 引入组件
+    import School from './School.vue'
+    import Student from './Student.vue'
+
+    //可省略Vue.extend 
+    export default {
+        name:'App',
+        components:{
+            School,
+            Student
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ Vue2/19-单文件组件/Student.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <!-- 组件的结构 -->
+        <h2>姓名：{{name}}</h2>  
+        <h2>年龄：{{age}}</h2>  
+        <button @click="welcome">点我欢迎</button>
+    </div>
+</template>
+  
+<script>
+    // 组件的交互相关代码（数据，方法等）
+    export default Vue.extend({
+        name:'Student',
+        data() {
+            return {
+                name:'李四',
+                age:20
+            }
+        },
+        methods: {
+            welcome(){
+                alert(`欢迎${this.name}的到来！`)
+            }
+        },
+    })
+</script>
+
+<style>
+    /* 组件的样式 */
+</style>
