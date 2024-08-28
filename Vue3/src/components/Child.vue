639ce32255572a20ea2392caff2211f4d30b9c6d
ChangChun <88582433+1801142494@users.noreply.github.com>
2024-08-29 00:10:57 +0800

    Add files via upload

---
+++ Vue3/src/components/Child.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="child">
+    <h2>Child组件</h2>
+    {{sum}}
+  </div>
+</template>
+
+<script>
+    export default {
+        name:'Child',
+        setup(){
+          let sum=0
+
+          return new Promise((resolve,reject)=>{
+            setTimeout(()=>{
+              // 等待三秒返回
+              resolve({sum})
+            },1000)
+          })
+        }
+    }
+</script>
+
+<style scope>
+ .child{
+    background-color: skyblue;
+    padding: 10px;
+ }
+</style>
